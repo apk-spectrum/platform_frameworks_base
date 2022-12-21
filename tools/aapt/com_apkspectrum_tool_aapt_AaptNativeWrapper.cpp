194b05a97ba80966fb2fb7051339213c9bdcf489
Sunggyu Kam <sunggyu.kam@gmail.com>
2022-12-21 17:34:25 +0900

    Support a local character of file name on windows
    Change-Id: I2d404aa28bfef87a26395667b0f524e18698f5c2

--- tools/aapt/com_apkspectrum_tool_aapt_AaptNativeWrapper.cpp
+++ tools/aapt/com_apkspectrum_tool_aapt_AaptNativeWrapper.cpp
@@ -12,92 +12,76 @@
 extern int main(int argc, char* const argv[]);
 
 JNIEXPORT jobjectArray JNICALL Java_com_apkspectrum_tool_aapt_AaptNativeWrapper_run
-  (JNIEnv *env, jclass /*thiz*/, jobjectArray params)
-{
-    char prog[8] = "libaapt";
+  (JNIEnv *env, jclass /*thiz*/, jobjectArray params) {
+    char prog[] = "libaapt";
     jsize paramCnt = 0;
-    
+
     if(params != NULL) {
         paramCnt = env->GetArrayLength(params);
     }
     if(paramCnt < 0) paramCnt = 0;
 
-	char* buf[paramCnt+1];
-	buf[0] = prog;
+	char* argv[paramCnt+3];
+	argv[0] = prog;
 
-    jstring encoding = getStickyEncodingCharacterSet(env);
-    jclass java_lang_String = NULL;
-    jmethodID java_lang_String_getBytes = NULL;
-
-    if(encoding != NULL) {
-        java_lang_String = env->FindClass("java/lang/String");
-        if(java_lang_String != NULL) {
-            java_lang_String_getBytes = env->GetMethodID(java_lang_String, "getBytes", "(Ljava/lang/String;)[B");
-        }
-        if(java_lang_String_getBytes == NULL) {
-            encoding = NULL;
-            if(java_lang_String != NULL) {
-                env->DeleteLocalRef(java_lang_String);
-                java_lang_String = NULL;
-            }
-        }
-    }
-
+    int argc = 1;
+#ifdef _WIN32
+    bool isFileName = false;
+    bool checked = false;
+#endif
     for(int i = 0; i < paramCnt; i++) {
         jstring param = static_cast<jstring>(env->GetObjectArrayElement(params, i));
         if(param == NULL) {
-            buf[i+1] = NULL;
             fprintf(stderr, "params[%d] is NULL\n", i);
             continue;
         }
-
-        if(encoding != NULL) {
-            jbyteArray bytes = static_cast<jbyteArray>(env->CallObjectMethod(param, java_lang_String_getBytes, encoding));
-            // check UnsupportedEncodingException
-            if(env->ExceptionCheck()) {
-                fprintf(stderr, "UnsupportedEncodingException occurred\n");
-                env->ExceptionClear();
-                
-                jmethodID java_lang_String_getBytes_ = env->GetMethodID(java_lang_String, "getBytes", "()[B");
-                if(java_lang_String_getBytes_ != NULL) {
-                    bytes = static_cast<jbyteArray>(env->CallObjectMethod(param, java_lang_String_getBytes_));
-                }
+        char *str = jstring2cstr(env, param);
+        argv[argc++] = str;
+#ifdef _WIN32
+        if (isFileName && !checked) {
+            checked = true;
+            char *fileName = argv[--argc];
+            char *what = argv[--argc];
+            char *utfpath = jstring2utfstr(env, param);
+            if (strcmp(fileName, utfpath) != 0) {
+                char *opt = (char *) malloc(11);
+                sprintf(opt, "--utf8name");
+                argv[argc++] = opt;
+                argv[argc++] = utfpath;
+            } else if (utfpath != NULL) {
+                free(utfpath);
             }
-            if(bytes != NULL) {
-                buf[i+1] = jbyteArray2cstr(env, bytes);
-                env->DeleteLocalRef(bytes);
-            } else {
-                buf[i+1] = NULL;
-                fprintf(stderr, "Failure: encoding buf[%d] is NULL\n", i);
-            }
-        } else {
-            const char *jchar = env->GetStringUTFChars(param, 0);
-            buf[i+1] = (char *)malloc(4096);
-            if(buf[i+1] != NULL) {
-                sprintf(buf[i+1], "%s", (char*)jchar);
-            }
-            env->ReleaseStringUTFChars(param, jchar);
+            argv[argc++] = what;
+            argv[argc++] = fileName;
+        } else if(!isFileName) {
+            isFileName = strcmp(str, "strings") == 0
+                    || strcmp(str, "badging") == 0
+                    || strcmp(str, "permissions") == 0
+                    || strcmp(str, "resources") == 0
+                    || strcmp(str, "configurations") == 0
+                    || strcmp(str, "xmltree") == 0
+                    || strcmp(str, "xmlstrings") == 0;
         }
-
+#endif
         env->DeleteLocalRef(param);
     }
 
     jobjectArray stringArray = NULL;
     {
         OutLineBuffer olb(env);
-    	main(paramCnt+1,(char**)buf);
+    	main(argc, (char**) argv);
     	stringArray = olb.toArray();
     }
 
-    for(int i = 0; i < paramCnt; i++) {
-        if(buf[i+1] != NULL) {
-            free((void*)buf[i+1]);
+    for(int i = 1; i < argc; i++) {
+        if(argv[i] != NULL) {
+            free((void*)argv[i]);
         }
     }
 
     fflush(stdout);
     fflush(stderr);
-    
+
     return stringArray;
 }
 
@@ -109,9 +93,9 @@ JNIEXPORT jobjectArray JNICALL Java_com_apkspectrum_tool_aapt_AaptNativeWrapper_
 /*
 int jniRegisterNativMethod(JNIEnv* env, const char* className, const JNINativeMethod* gMethods, int numMethods ) {
     jclass clazz;
- 
+
     clazz = env->FindClass(className);
- 
+
     if(clazz == NULL){
         return -1;
     }
@@ -138,9 +122,9 @@ jint JNI_OnLoad(JavaVM* jvm, void* /*reserved*/) {
     }
 
     getStickyEncodingCharacterSet(env);
- 
+
     //jniRegisterNativMethod(env, "com/apkspectrum/tool/aapt/AaptNativeWrapper", sMethod, NELEM(sMethod));
- 
+
     return JNI_VERSION_1_6;
 }
 #endif
@@ -150,10 +134,10 @@ void JNI_OnUnload(JavaVM *jvm, void* /*reserved*/)
     JNIEnv *env;
 
     if (jvm->GetEnv((void **)&env, JNI_VERSION_1_6)) {
-        return; 
+        return;
     }
 
     releaseStickyEncodingCharacterSet(env);
 
-    return; 
+    return;
 }
